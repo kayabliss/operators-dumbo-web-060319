d808e027a34e8f10de49c2d894b45f6eac99ebdb
Ana Pichardo <kayablissfilms@gmail.com>
2019-05-31 05:02:12 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,10 +1,13 @@
 def unsafe?(speed)
- if speed <= 30 
+ if speed <= 30 || speed >= 60
+    "unsafe"
+else
+  "Safe"
 
 
 
 def not_safe?(speed)
-	
+	speed <= 30 || speed >=60 ? "unsafe" : "safe"
 end
 	
 
